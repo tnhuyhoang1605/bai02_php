1383d27d23dcd095c4ab4ab0dfc703be0fde8145
tnhuyhoang1605 <157784538+tnhuyhoang1605@users.noreply.github.com>
2024-01-26 13:47:44 +0700

    Add files via upload

---
+++ bai02_php/index.php
@@ -0,0 +1,26 @@
+<?php
+$x = 10.365;
+var_dump($x)
+?><?php
+$a = 5;
+echo $a; 
+?>
+<?php
+$a = 5;
+$A = 10;
+echo $a; 
+echo '<br>'; 
+echo $A; 
+?><?php
+$str = 'Hello World';
+echo '<br>'; 
+echo $str;
+?><?php
+$firstname = 'nguyen';
+$lastname = 'Van an';
+$fullname = $firstname .' '. $lastname;
+echo '<br>';
+echo $fullname;
+?><?php
+$cars = array('Toyota', 'Bmw', 'Volvo');
+?>
